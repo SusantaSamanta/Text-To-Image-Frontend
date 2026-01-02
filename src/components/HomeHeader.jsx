fae160fbcf1db9b1e52748425d9ceaeeab703cbf
Susanta Samanta <160142120+SusantaSamanta@users.noreply.github.com>
2026-01-02 11:58:47 +0530

    Image fix

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom';
+import profile1 from "../media/ForAboutPage/profile1.jpg";
+import ai_star_png from "../src/assets/ai_star.png";
 
 const HomeHeader = () => {
     return (
@@ -24,7 +26,7 @@ const HomeHeader = () => {
                         text-base lg:text-xl font-bold rounded-2xl lg:shadow-[5px_5px_10px_#4a4a4a72] hover:bg-blue-700 
                         hover:shadow-lg hover:scale-[1.02] hover:lg:lg:shadow-[5px_5px_10px_#4a4a4a72]  transition-all duration-200 ease-in-out">
                         Generate Image
-                        <img className='md:w-[30px] border-0 inline' src='../src/assets/ai_star.png' />
+                        <img className='md:w-[30px] border-0 inline' src={ai_star_png} />
                     </button>
                 </Link>
             </motion.section>
